0c06221dd3e3edd1ad3ba551f8ac19e49a10f9a1
Evgeny Belov <e.belov@inbox.ru>
2024-03-24 21:39:37 +0300

    create Comment added

--- src/main/kotlin/ru/netology/posts.kt
+++ src/main/kotlin/ru/netology/posts.kt
@@ -10,7 +10,8 @@ data class Post(
     val date: Int = 1710410475,
     val friendOnly: Boolean = false,
     val canDelete: Boolean = false,
-    val attachments: Array<Attachment>? = null
+    val attachments: Array<Attachment>? = null,
+    val comments: Array<Comment>? = null
 )
 
 
@@ -23,14 +24,60 @@ data class Likes(
 
 )
 
+data class Comment(
+    val id: Int,
+    val text: String,
+    val fromId: Int = 0,
+    val date: Int = 1710415475,
+    val donut: Donut? = null,
+    val replyToUser: Int = 0,
+    val replyToComment: Int = 0,
+    val attachments: Array<Attachment>? = null,
+    val parentsStack: Array<ParentsId>? = null,
+    val thread: Thread? = null,
+
+
+    ) {
+    class Donut(
+        val isDon: Boolean,
+        val placeholder: String
+    )
+
+    class ParentsId(val parentsId: Int)
+    class Thread(
+        val count: Int,
+//        val items:Array<Items>, TODO if necessary
+        val canPost: Boolean,
+        val showReplyButton: Boolean,
+        val groupsCanPost: Boolean
+
+    )
+}
+
+class PostNotFoundException(message: String) : RuntimeException(message)
+
 
 object WallService {
     private var id = 11357
 
     private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
+
+
+    fun createComment(postId: Int, comment: Comment): Comment {
+        for (post in posts) {
+            if (post.id == postId) {
+                comments += comment
+                return comments.last()
+            }
+
+        }
+        return throw PostNotFoundException("Post ID $postId doesn't exist")
+
+    }
 
     fun add(post: Post): Post {
-        posts += post.copy(id = id, likes = post.likes?.copy())
+        posts += post.copy(id = id, likes = post.likes?.copy(), comments = post.comments?.copyOf())
         id++
         return posts.last()
     }
@@ -51,10 +98,34 @@ object WallService {
         id = 11357
     }
 
+    fun printPosts() {
+        for (post in posts) {
+            println("ID: ${post.id} Post: ${post.text} Comment: ${post.comments?.get(0)?.text}")
+        }
+    }
+
+    fun printComment() {
+        for ((index) in comments.withIndex()) {
+            println("$index text: ${comments.get(index).text}")
+        }
+    }
+
 
 }
 
+
 fun main() {
-//    print("Hello")
+    val post1 = Post("Hello")
+    val post2 = Post("Bonjour")
+    val post3 = Post("Ni Hao")
+    val comment1 = Comment(1, "Comment added")
+    WallService.add((post1))
+    WallService.add(post2)
+    WallService.add(post3)
+    WallService.createComment(11359, comment1)
+//    WallService.createComment(11358, comment1)
+
+    WallService.printComment()
+
 
 }
